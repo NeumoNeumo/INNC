1a6ab4ff55453dfee3ba47efbb08ce0a3f3ce25a
Jianhui Yuan Xu <neuromoments@gmail.com>
2023-12-03 17:16:39 +0800

    add sphinx and doxygen

---
+++ docs/conf.py
@@ -0,0 +1,7 @@
+from cgitb import html
+
+extensions = ["breathe"]
+
+html_theme = "sphinx_rtd_theme"
+
+breathe_default_project = "libtensor"
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+libtensor Documentation
+=======================
+
+.. toctree::
+  :maxdepth: 2
+
+C++ classes
+-----------
+
